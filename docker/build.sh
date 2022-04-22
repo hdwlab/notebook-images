82a0c9c07d2ed5c469d946688fc3e742b951055f
d-hayashi <daiki.hayashi@hdwlab.co.jp>
2022-04-22 22:30:54 +0900

    Add CI

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,9 @@ WORKSPACE_ROOT="$SCRIPT_DIR/../"
 # https://github.com/docker/buildx/issues/484
 export BUILDKIT_STEP_LOG_MAX_SIZE=10000000
 
-docker buildx bake --load --progress=plain -f "$SCRIPT_DIR/docker-bake.hcl" \
+${PUSH_IMAGE:-false} && loadOrPush="--push" || loadOrPush="--load"
+
+set -x
+docker buildx bake ${loadOrPush} --progress=plain -f "$SCRIPT_DIR/docker-bake.hcl" \
     --set "*.context=$WORKSPACE_ROOT" \
     --set "*.ssh=default"
