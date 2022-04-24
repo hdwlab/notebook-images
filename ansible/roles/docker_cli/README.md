e2f435b707ab618e9f4597ddab716cede9696fe4
d-hayashi <daiki.hayashi@hdwlab.co.jp>
2022-04-24 12:45:48 +0900

    Refactoring

---
+++ ansible/roles/docker_cli/README.md
@@ -0,0 +1,4 @@
+# docker_cli
+
+Install docker-cli.
+
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 
-# Start SSH Server
-ENABLE_SSH_SERVER=true
-if ${ENABLE_SSH_SERVER}; then
-  [[ ! -d /run/sshd ]] && mkdir /run/sshd
-  /usr/sbin/sshd
-fi
-
-# Enable docker
-DOCKER_SOCKET=/var/run/docker.sock
-if [[ -S ${DOCKER_SOCKET} ]]; then
-    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
-    DOCKER_GROUP=$(getent group ${DOCKER_GID} | awk -F ":" '{ print $1 }' || echo "")
-    if [[ ${DOCKER_GROUP} ]]; then
-        addgroup ${NB_USER} ${DOCKER_GROUP}
-    else
-        addgroup --system --gid ${DOCKER_GID} docker
-        usermod -aG docker ${NB_USER}
-    fi
-fi
-
 if [[ $# -eq 0 ]]; then
     /docker-entrypoint-internal.sh /init
 else
